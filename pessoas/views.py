70e528a0db62908bf839b116ecb38e19bf91f0bc
polianacavazini <polianacavazini@gmail.com>
2014-04-04 21:56:02 -0300

    Segundo commit de todo o projeto

--- pessoas/views.py
+++ pessoas/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from pessoas.models import Pessoa
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
