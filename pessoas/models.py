f342e694b7a451540707cb0daa74bce438d67658
polianacavazini <polianacavazini@gmail.com>
2014-04-11 20:33:29 -0300

    Enviando Informacoes

--- pessoas/models.py
+++ pessoas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length='100', 
+        blank=False, 
+        null=False)
+
+    email = models.CharField(max_length='100', 
+        blank=True, 
+        null=True)
+
+    telefone = models.CharField(max_length='20', 
+        blank=True, 
+        null=True)
+
+    logradouro = models.CharField(max_length='200', 
+        blank=True,
+        null=True)
